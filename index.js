fad397e9ff6fb37ed86b5b6624621092a8e643c1
unknown <abdelhedi.hlel@oyez.fr>
2018-12-04 14:13:45 +0100

    adding simple Querries and Mutations

--- index.js
+++ index.js
@@ -1,52 +1,38 @@
 const { ApolloServer, gql } = require('apollo-server')
 
-// This is a (sample) collection of books we'll be able to query
-// the GraphQL server for.  A more complete example might fetch
-// from an existing data source like a REST API or database.
-const books = [
-  {
-    title: 'Harry Potter and the Chamber of Secrets',
-    author: 'J.K. Rowling'
-  },
-  {
-    title: 'Jurassic Park',
-    author: 'Michael Crichton'
-  }
+const todos = [
+  { task: 'swiming ', completed: true }
 ]
 
-// Type definitions define the "shape" of your data and specify
-// which ways the data can be fetched from the GraphQL server.
 const typeDefs = gql`
-  # Comments in GraphQL are defined with the hash (#) symbol.
-
-  # This "Book" type can be used in other type declarations.
-  type Book {
-    title: String
-    author: String
+  type Todo {
+    task : String
+    completed : Boolean
   }
-
-  # The "Query" type is the root of all GraphQL queries.
-  # (A "Mutation" type will be covered later on.)
   type Query {
-    books: [Book]
+    getTodos: [Todo]
+  }
+  type Mutation {
+    addToDo(task : String,completed : Boolean): Todo
   }
 `
 
-// Resolvers define the technique for fetching the types in the
-// schema.  We'll retrieve books from the "books" array above.
 const resolvers = {
   Query: {
-    books: () => books
+    getTodos: () => todos
+  },
+  Mutation: {
+    addToDo: (_, { task, completed }) => {
+      const newTodo = { task, completed }
+      todos.push(newTodo)
+      // console.log('ur todos', todos)
+      return newTodo
+    }
   }
 }
 
-// In the most basic sense, the ApolloServer can be started
-// by passing type definitions (typeDefs) and the resolvers
-// responsible for fetching the data for those types.
 const server = new ApolloServer({ typeDefs, resolvers })
 
-// This `listen` method launches a web-server.  Existing apps
-// can utilize middleware options, which we'll discuss later.
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`)
 })
