fc37da79b0865f85fdb9869eeee0726578930fd7
abdelhedi <abdelhedihlel@gmail.com>
2018-12-04 22:46:23 +0100

    adding models

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
-const { ApolloServer, gql } = require('apollo-server')
+const { ApolloServer } = require('apollo-server')
 const mongoose = require('mongoose')
+require('dotenv').config({ path: 'variables.env' })
 
+const User = require('./models/User')
+const Todo = require('./models/Todo')
+
+// for gql files
+const fs = require('fs')
+const path = require('path')
+const filePath = path.join(__dirname, 'typeDefs.gql')
+const typeDefs = fs.readFileSync(filePath, 'utf-8')
 mongoose
-  .connect(process.env.MONGO_URL, { useNewUrlParser: true })
-  .then(() => console.log('database connected'))
+  .connect(process.env.MONGO_DB_URL, { useNewUrlParser: true })
+  .then(() => console.log('DB connected'))
   .catch(err => console.log(`error : ${err}`))
-const typeDefs = gql`
-  type Todo {
-    task : String
-    completed : Boolean
-  }
-  type Query {
-    getTodos: [Todo]
-  }
-`
 
-const server = new ApolloServer({ typeDefs })
+const server = new ApolloServer({
+  typeDefs,
+  context: {
+    User,
+    Todo
+  }
+})
 
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`)
